a444cbfbfca1213987ccc9a68de2af601908c974
Serhii Lukashchuk <s.lukaschuk@yahoo.com>
2025-01-12 01:37:50 -0500

    Initial Commit

---
+++ WindowsAPlsApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WindowsAPlsApp
+//
+//  Created by Serhii Lukashchuk on 2025-01-12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WindowsAPlsApp/WindowsAPlsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WindowsAPlsAppApp.swift
+//  WindowsAPlsApp
+//
+//  Created by Serhii Lukashchuk on 2025-01-12.
+//
+
+import SwiftUI
+
+@main
+struct WindowsAPlsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
