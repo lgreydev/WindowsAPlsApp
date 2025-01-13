8abe05667e9e48e2894ff9f14e7352f4ad52a0d6
Serhii Lukashchuk <s.lukaschuk@yahoo.com>
2025-01-13 04:10:18 -0500

    add hover and dismiss functionality to floating window

--- WindowsAPlsApp/ContentView.swift
+++ WindowsAPlsApp/ContentView.swift
@@ -19,11 +19,33 @@ struct ContentView: View {
 }
 
 struct FloatingWindow: View {
+    
+    @State private var isHovering: Bool = false
+    @Environment(\.dismissWindow) private var dismissWindow
+    
     var body: some View {
         Image(.pic)
             .resizable()
             .aspectRatio(contentMode: .fill)
             .frame(width: 200, height: 200)
+            .overlay(content: {
+                Rectangle()
+                    .fill(.ultraThinMaterial)
+                    .overlay(content: {
+                        Button {
+                            dismissWindow()
+                        } label: {
+                            Image(systemName: "xmark.circle.fill")
+                                .font(.largeTitle)
+                        }
+                        .buttonStyle(.plain)
+                    })
+                    .opacity(isHovering ? 1 : 0)
+                    .animation(.snappy, value: isHovering)
+            })
+            .onHover {
+                isHovering = $0
+            }
             .clipShape(.rect(cornerRadius: 30))
     }
 }
