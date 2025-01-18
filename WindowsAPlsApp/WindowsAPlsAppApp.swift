14838fddcc6bf89e25cc163a3a22a2c02c8be0d3
Serhii Lukashchuk <s.lukaschuk@yahoo.com>
2025-01-17 20:58:46 -0500

    add dismiss window

--- WindowsAPlsApp/WindowsAPlsAppApp.swift
+++ WindowsAPlsApp/WindowsAPlsAppApp.swift
@@ -23,5 +23,16 @@ struct WindowsAPlsAppApp: App {
         .windowLevel(.floating)
         .windowResizability(.contentSize)
         .windowStyle(.plain)
+        
+        WindowGroup(id: "AlertWindow") {
+            AlertWindows()
+                .allowsHitTesting(false)
+                .toolbarVisibility(.hidden, for: .windowToolbar)
+                .containerBackground(.clear, for: .window)
+        }
+        .windowLevel(.floating)
+        .windowBackgroundDragBehavior(.disabled)
+        .windowResizability(.contentSize)
+        .windowStyle(.plain)
     }
 }
